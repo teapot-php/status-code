ea4232a4788cda3891e19cd22b9e0f1425776951
Andreas Möller <am@localheinz.com>
2023-12-05 08:26:32 +0100

    Fix: Remove header

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * Copyright (c) 2017-2023 Andreas Möller.
- *
- * For the full copyright and license information, please view
- * the LICENSE.md file that was distributed with this source code.
- *
- * @see https://github.com/ergebnis/php-package-template
- */
-
 use Ergebnis\PhpCsFixer;
 
 $config = PhpCsFixer\Config\Factory::fromRuleSet(new PhpCsFixer\Config\RuleSet\Php80(), [
